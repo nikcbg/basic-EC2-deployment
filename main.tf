4bed9e9254f9edd95ece81193ee95002a96c37c1
nikcbg <nikcbg@yahoo.com>
2021-02-11 09:16:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-variable "access_key" {}
-variable "secret_key" {}
+# variable "access_key" {}
+# variable "secret_key" {}
 # variable "region" {}
 # variable "ami" {}
 # variable "instance_type" {}
