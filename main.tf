083f1c228e8496e31ee18437f9b55e5bb6427543
nikcbg <nikcbg@yahoo.com>
2021-03-19 07:41:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-# variable "access_key" {}
-# variable "secret_key" {}
+ variable "access_key" {}
+ variable "secret_key" {}
 # variable "region" {}
 # variable "ami" {}
 # variable "instance_type" {}
-# ami-085925f297f89fce1
+
 
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region = var.region
+  region = "us-east-1"
 }
 resource "aws_instance" "web123" {
-  ami           = var.ami
-  instance_type = var.instance_type
+  ami           = "ami-013f17f36f8b1fefb"
+  instance_type = t2.micro
   tags = {
     Name = "my-test-instance"
   }
