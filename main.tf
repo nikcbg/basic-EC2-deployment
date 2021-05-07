b4025e6a9c146da86ec1c0939512c957e38dc820
nikcbg <nikcbg@yahoo.com>
2021-05-07 13:50:53 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
- variable "access_key" {}
- variable "secret_key" {}
- variable "token" {}
+variable "access_key" {}
+variable "secret_key" {}
+variable "token" {}
 # variable "region" {}
 # variable "ami" {}
 # variable "instance_type" {}
-
 
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
   token      = var.token
-  region = "us-east-1"
+  region     = "us-east-1"
 }
 resource "aws_instance" "web123" {
-  ami           = "ami-0747bdcabd34c712a"
+  ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
   tags = {
     Name = "my-test-instance"
@@ -21,5 +20,5 @@ resource "aws_instance" "web123" {
 }
 
 output "public_dns" {
-value = aws_instance.web123.public_dns
+  value = aws_instance.web123.public_dns
 }
