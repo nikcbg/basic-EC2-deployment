bbbcd68fbe800a1257fd1d63527b710568e0c809
nikcbg <nikcbg@yahoo.com>
2021-06-16 09:26:47 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "aws" {
   token      = var.token
   region     = "us-east-1"
 }
-resource "aws_instance" "web123" {
+resource "aws_instance" "test" {
   ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
   tags = {
