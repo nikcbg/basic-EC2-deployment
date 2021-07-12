d81d4460310564adf6b26c43290cebd3a362e454
nikcbg <nikcbg@yahoo.com>
2021-07-12 09:16:51 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "token" {}
 
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
-  token      = var.token
   region     = "us-east-1"
 }
 resource "aws_instance" "test" {
