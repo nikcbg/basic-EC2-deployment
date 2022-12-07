474702b2e2c795718e2073858b43dcc8e5bc66ca
nikcbg <nikcbg@yahoo.com>
2022-12-07 12:54:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "test1" {
   ami           = "ami-0747bdcabd34c712a"
   instance_type = "t2.micro"
   tags = {
-    Name = "nik-test-instance-1"
+    Name = "nik-test-instance"
   }
 }
 
